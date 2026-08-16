4a02298de9a63f5add953759e9f3f97e4662ad32
ariefxaloka <31511038+ariefxaloka@users.noreply.github.com>
2026-08-16 22:32:06 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // KONFIGURASI - GANTI DENGAN URL WEB APP GOOGLE APPS SCRIPT KAMU
 // =========================================================
 const CONFIG = {
-APP_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyrdi1XQrfUd9xFZYdMf0JtIHss0BbF4n0wj3KVh-Baoe0LhjvwmeEDmVjj5tX1pvQbGw/exec'
+APP_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwpXKjwRPw6maZelDOD0fqMFD6cvKR6kW3NRdRpJ3V80vp-a9NhbFSC-W-veyX_Wt5CPw/exec'
 };
 
 // ---------------------- AUTH (PIN admin) ----------------------
