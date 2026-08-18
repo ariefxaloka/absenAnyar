1390515efe3c941b587375a1e9ee28b76f46d294
ariefxaloka <31511038+ariefxaloka@users.noreply.github.com>
2026-08-18 17:41:21 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // KONFIGURASI - GANTI DENGAN URL WEB APP GOOGLE APPS SCRIPT KAMU
 // =========================================================
 const CONFIG = {
-  APP_SCRIPT_URL: 'PASTE_URL_WEB_APP_APPS_SCRIPT_DI_SINI'
+  APP_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzyFP_ECgMMcMWUvAhiUAzSFkz89CO2DwhZP-bW0YUxTFGff2XGDLrwKbdbQY4_rd69ew/exec'
 };
 
 const BLOK_OPTIONS = { JOLIN: ['F', 'G'], PIRES: ['A', 'B', 'C', 'D', 'E'] };
